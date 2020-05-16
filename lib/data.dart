3706f98de8c26034c02aa3508b61735cda1fea14
ALKA SEBASTIAN <alka.lizzy@gmail.com>
2020-05-16 23:28:00 +1000

    second commti

---
+++ lib/data.dart
@@ -0,0 +1,18 @@
+var images = [
+  "images/black.png",
+  "images/blackankle.png",
+  "images/charcoal.png",
+  "images/navy.png",
+  "images/redankle.png",
+  "images/white.png",
+];
+
+var title = [
+  "Chuck Taylor All Star Classic Color Low top Black",
+  "Chuck Taylor All Star Classic Color High Top Black",
+  "Chuck Taylor All Star Classic Color High Top Charcoal",
+  "Chuck Taylor All Star Classic Color High Top Navy",
+  "Chuck Taylor All Star Classic Color High Top Red",
+  "Chuck Taylor All Star Classic Color Low Top White"
+];
+var price = ["120", "120", "120", "120", "120", "120"];
